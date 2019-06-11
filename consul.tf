ab20697b3b651bafa3991d5aa57b55395da81173
neshimbekova <neshimbekova@gmail.com>
2019-06-10 21:54:23 -0500

    sdf

--- consul.tf
+++ consul.tf
@@ -22,18 +22,17 @@ resource "aws_instance" "consul" {
     }
   }
 
-  connection {
-    host        = "${self.public_ip}"
-    type        = "ssh"
-    user        = "${var.user}"
-    private_key = "${file(var.ssh_key_location)}"
-  }
+  # connection {
+  #   host        = "${self.public_ip}"
+  #   type        = "ssh"
+  #   user        = "${var.user}"
+  #   private_key = "${file(var.ssh_key_location)}"
+  # }
 
   provisioner "file" {
     source      = "consul.service"
     destination = "/tmp/consul.service"
   }
-
   provisioner "remote-exec" {
     # connection {  #   host        = "${self.public_ip}"  #   type        = "ssh"  #   user        = "${var.user}"  #   private_key = "${file(var.ssh_key_location)}"    # }
 
